2bb579773bd72fcdd542c1b6e66d06f5cbd69803
Roman <74704965+KengiTakery0@users.noreply.github.com>
2025-09-19 13:49:58 +0300

    Add samle structure and User Entity

---
+++ src/Space.Backend/Models/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Space.Backend.Models.Enteties
+{
+
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [Required]
+        int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        string Password_Hash { get; set; }
+
+    }
+}
