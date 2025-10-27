9a5ac5cf90342cc9a1427587bb900cb8c2ce541f
Roman <74704965+KengiTakery0@users.noreply.github.com>
2025-10-28 02:33:55 +0300

    Исправления интерфейса авторизации

--- src/Space.Service.Autorization/Services/Interfaces/IAuthService.cs
+++ src/Space.Service.Autorization/Services/Interfaces/IAuthService.cs
@@ -1,10 +1,16 @@
-﻿using Space.Service.Autorization.Data.Repositories.Implimentations;
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Space.Service.Autorization.Data.Repositories.Implimentations;
+using Space.Service.Autorization.Models.DTOs.Response;
 using Space.Service.Autorization.Models.Entity;
 
 namespace Space.Service.Autorization.Services.Interfaces
 {
     public interface IAuthService
     {
-       
+        Task<AuthResponse> RegisterAsync(RegisterRequest request);
+        Task<LoginResponse> LoginAsync(LoginRequest request);
+        Task<LoginResponse> RefreshTokenAsync(string refreshToken);
+        Task<bool> RevokeTokenAsync(string refreshToken);
+        Task<bool> LogoutAsync(Guid userId, string refreshToken);
     }
 }
