0a8caa871d3ed12905311be96faa0bfc3c11207d
Roman <74704965+KengiTakery0@users.noreply.github.com>
2025-10-27 11:04:32 +0300

    Изменение токен репозитория

--- src/Space.Service.Autorization/Data/Repositories/Interfaces/ITokenRepository.cs
+++ src/Space.Service.Autorization/Data/Repositories/Interfaces/ITokenRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace Space.Service.Autorization.Data.Repositories.Interfaces
+﻿using Space.Service.Autorization.Models.Entity;
+
+namespace Space.Service.Autorization.Data.Repositories.Interfaces
 {
     public interface ITokenRepository
     {
-       
+        Task<RefreshToken?> GetByTokenAsync(string token);
+        Task<IEnumerable<RefreshToken>> GetByUserIdAsync(Guid userId);
+        Task AddAsync(RefreshToken refreshToken);
+        Task RevokeTokenAsync(string token);
+        Task RevokeAllUserTokensAsync(Guid userId);
+        Task<bool> IsTokenValidAsync(string token);
+        Task CleanExpiredTokensAsync();
+        Task SaveChangesAsync();
     }
 }
--- src/Space.Service.Autorization/Services/Interfaces/ITokenService.cs
+++ src/Space.Service.Autorization/Services/Interfaces/ITokenService.cs
@@ -1,6 +1,15 @@
-﻿namespace Space.Service.Autorization.Services.Interfaces
+﻿using Space.Service.Autorization.Data.Repositories.Implimentations;
+using Space.Service.Autorization.Models.Entity;
+
+namespace Space.Service.Autorization.Services.Interfaces
 {
     public interface ITokenService
     {
+        string GenerateJwtToken(User user);
+        RefreshToken GenerateRefreshToken();
+        bool ValidateRefreshToken(string token);
+
+        Guid? GetUserIdFromToken(string token);
+        User? GetUserInfoFromToken(string token);
     }
 }
